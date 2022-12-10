76785d5ff8ac2120da181acef324776d02aa422d
Divyesh <userofficial00@gmail.com>
2022-12-10 12:58:15 +0530

    Abs-Encap

---
+++ Polymorphism/Overloading2.java
@@ -0,0 +1,15 @@
+package Polymorphism;
+
+public class Overloading2 {
+    public static void main(String[] args) {
+        OverLoading overLoading = new OverLoading();
+        //Call method without Argument
+
+        overLoading.display();
+        System.out.println(" \n ");
+
+        overLoading.display('@');
+
+    }
+
+}
